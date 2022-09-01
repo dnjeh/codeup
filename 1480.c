e047574804be37c37361b2bbfc5c507c19c91042
dnjeh <ehdnj.001@gmail.com>
2022-09-01 22:54:12 +0900

    today's low-range codeups'

--- 1480.c
+++ 1480.c
@@ -3,16 +3,17 @@ int main() {
     int i, j, a, b, t=1;
     scanf("%d %d", &a, &b);
     int c[a][b];
-    for(i=0;a*b>=t;i++) {
-        for(j=i;(b-1-i+j)>=0&&j>=0;j--) {
-            if(j>=a) {
+    for(i=a-1;a*b>=t;i--) {
+        for(j=i;b+i-j-1>=0&&j<a;j++) {
+            if(j<0) {
                 continue;
             }
-            else {  
-                c[j][(b-1-i+j)]=t++;
+            else {
+                c[j][b+i-j-1]=t++;
             }
         }
     }
+
     for(i=0;i<a;i++) {
         for(j=0;j<b;j++) {
             printf("%d ", c[i][j]);
