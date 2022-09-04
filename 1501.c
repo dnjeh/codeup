fc3cc2ab6be8d642ae568252db5b71682ede3cde
dnjeh <ehdnj.001@gmail.com>
2022-09-04 20:43:20 +0900

    Just a little bit of codeups'

---
+++ 1501.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a, i, j, t=1;
+    scanf("%d", &a);
+    int b[a][a];
+    for(i=0;i<a;i++) {
+        for(j=0;j<a;j++) {
+            b[i][j]=t++;
+        }
+    }
+    for(i=0;i<a;i++) {
+        for(j=0;j<a;j++) {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+}
