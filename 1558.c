d60c580995038ad71d26b0a836ff7440ad7634ec
dnjeh <ehdnj.001@gmail.com>
2022-09-18 21:19:20 +0900

    Anyway, today's codeup's

--- 1558.c
+++ 1558.c
@@ -4,14 +4,14 @@
 long long int n;
 long long int f(long long int n) {
     long long int m=0LL, sum=1LL, i, j;
-    for(i=1LL;(n/sum)!=0LL;i++) {
+    for(i=1LL;(n/sum)!=0LL&&i<=19;i++) {
         sum*=10LL;
     }
     i--;
     for(j=i;j>0LL;j--) {
-        m+=((n/(long long int)(pow(10LL, (i-j))))%10LL)*(long long int)(pow(10LL, j));
+        m+=((n/(long long int)(pow(10LL, (i-j))))%10LL)*(long long int)(pow(10LL, j-1));
     } 
-    return (long long int)m/10;
+    return (long long int)m;
 }// 이 부분에 들어가야 될 코드를 작성하여 제출
 int main()
 {
