49cee855ab91c591d5dd54700bb38be81035ca92
dnjeh <ehdnj.001@gmail.com>
2022-09-30 13:53:44 +0900

    Today's noon-extra codeup's

--- 3011.c
+++ 3011.c
@@ -7,7 +7,7 @@ int main() {
         scanf("%d", &a[i]);
     }
     for(i=0; i<n; i++) {
-        for(j=0; j<n-i; j++) {
+        for(j=0; j<n-i-1; j++) {
             if (a[j] > a[j+1]) {
                 temp = a[j];
                 a[j] = a[j+1];
@@ -18,10 +18,11 @@ int main() {
             if(a[j-1]>a[j]) {
                 break;
             }
-            if(j+1==n) {
-                printf("%d", i+1);
-                return 0;
-            }
+            
+        }
+        if(j==n) {
+            printf("%d", i+1);
+            return 0;
         }
     }
     printf("%d", n-1);
