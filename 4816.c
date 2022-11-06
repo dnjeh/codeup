17e865cd9e98027b0c4f02eaeaa9853a0bef8379
dnjeh <ehdnj.001@gmail.com>
2022-11-06 19:31:14 +0900

    Today's non-solved included cdeups'

---
+++ 4816.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int t;
+    scanf("%d", &t);
+    if(t%10) {
+        printf("-1");
+    }
+    else {
+        printf("%d %d %d", t/300, (t%300)/60, (t%60)/10);
+    }
+}
