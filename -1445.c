94215bcf01762ee53a6935fdfe247ccb5f62bcc0
dnjeh <ehdnj.001@gmail.com>
2022-11-12 22:30:23 +0900

    Today's 2-stack-un-solved codeup's

--- -1445.c
+++ -1445.c
@@ -3,30 +3,27 @@ int main() {
     int n, m, i, a1=0, b1=0;
     scanf("%d %d", &n, &m);
     int a[n], b[m], c[n+m];
-    for(i=0;i<n+m;i++) {
-        c[i]=0;
-    }
     for(i=0;i<n;i++) {
         scanf("%d", &a[i]);
     }
     for(i=0;i<m;i++) {
         scanf("%d", &b[i]);
     }
-    for(i=0;i<n+m;i++) {
-        if(a[a1]<b[b1]) {
+    for(i=0;i<n+m;i++) { 
+        if((a[a1]<b[b1]||(a[a1]<b[b1]&&b1+1==m))&&a1<n) {
             c[i]=a[a1];
             a1++;
         }
-        else if(a[a1]>b[b1]) {
+        else if(a[a1]>b[b1]||a1+1>=n) {
             c[i]=b[b1];
             b1++;
         }
         else {
-            if(a1+1==n) {
+            if(n-a1<m-b1){
                 c[i]=b[b1];
                 b1++;
             }
-            else {
+            else if(n-a1>=m-b1){
                 c[i]=a[a1];
                 a1++;
             }
