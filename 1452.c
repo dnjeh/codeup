cab2a56208eb7dcba3e5c282cc22ba7504012699
dnjeh <ehdnj.001@gmail.com>
2023-03-22 21:24:55 +0900

    Today's fimmmmmmalllly completed codeup's sort's

--- 1452.c
+++ 1452.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
+
+void merge(int arr[], int left, int middle, int right) {
+    int i, j, k;
+    int n1 = middle-left+1;
+    int n2 = right-middle;
+    int L[n1], R[n2];
+    for(i=0;i<n1;i++) L[i]=arr[left+i];
+    for(j=0;j<n2;j++) R[j]=arr[middle+1+j];
+    for(i=0,j=0,k=left;i<n1&&j<n2;k++) {
+        if(L[i]<=R[j]) arr[k]=L[i++];
+        else arr[k]=R[j++];
+    }
+    for(;i<n1;i++,k++) arr[k]=L[i];
+    for(;j<n2;j++,k++) arr[k] = R[j];
+}
+ 
+void mergeSort(int arr[], int left, int right) {
+    if(left<right) {
+        int middle = left + (right - left) / 2;
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+        merge(arr, left, middle, right);
+    }
+}
 int main() {
-    int n, i, j, temp, key;
+    int n, i, j;
     scanf("%d", &n);
     int a[n];
     for (i=0;i<n;i++) {
         scanf("%d", &a[i]);
     }
-    
+    mergeSort(a, 0, n-1);
     for (i=0;i<n;i++)
         printf("%d\n", a[i]);
     return 0;
